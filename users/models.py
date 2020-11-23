23e2372fcd285990054165c4b2640f55393b3537
Roman Krasnikov <rkrasnikov23@gmail.com>
2020-11-23 23:26:56 +0300

    update user model

--- users/models.py
+++ users/models.py
@@ -4,4 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-
+class User(AbstractUser):
+    subscribers = models.ForeignKey(AbstractUser, on_delete=models.CASCADE, related_name='subscribers')
